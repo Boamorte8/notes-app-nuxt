9f6164102ed60da05b22734e3dff062a82d19750
Esteban Salazar Zapata <esteban.salazar@Esteban-Salazar-MacBook-Pro.local>
2022-09-23 21:31:55 -0500

    feat(login) implement login form

---
+++ components/Auth/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <NCard class="card">
+      <h2 class="title">Login</h2>
+      <div class="inputs">
+        <input class="input" type="email" placeholder="Email">
+        <input class="input" type="password" placeholder="Password">
+      </div>
+      <NButton>Login</NButton>
+    </NCard>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  padding: 1.5rem;
+  width: 25rem;
+}
+
+.title {
+  font-size: 1.75rem;
+  margin-bottom: .75rem;
+}
+
+.inputs {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  margin-bottom: .9rem;
+  padding: .25rem;
+  outline: none;
+  border: .1rem solid rgba(0, 0, 0, .1);
+  border-radius: .25rem;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="body">
+    <div class="container">
+      <AuthCard />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.body {
+  display: flex;
+  justify-content: center;
+}
+
+.container {
+  max-width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
